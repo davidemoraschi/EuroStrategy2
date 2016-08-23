411081f1401ff643f5742a929a6a83e2df0fe2ef
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 17:31:41 +0200

    moved to GitHub

---
+++ TokenHelperOwin/TokenHelperRole/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using System.Web.Http;
+
+
+namespace TokenHelperRole
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "Default",
+                "{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ TokenHelperOwin/TokenHelperRole/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenHelperRole
+{
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Hola from PANDAS!")
+            };
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            string msg = String.Format("Hola from PANDAS (id = {0})", id);
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(msg)
+            };
+        }
+    }
+}
